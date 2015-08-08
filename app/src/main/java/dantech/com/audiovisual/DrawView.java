e2227fdd976da2978c06cd4e309a058109d69072
Dan <dangarbage15@yahoo.com>
2015-08-08 18:14:44 -0500

    added visulaizer and radial gradient

--- app/src/main/java/dantech/com/audiovisual/DrawView.java
+++ app/src/main/java/dantech/com/audiovisual/DrawView.java
@@ -1,7 +1,13 @@
 package dantech.com.audiovisual;
 
 import android.content.Context;
-import android.util.AttributeSet;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
 import android.view.View;
 
 /**
@@ -9,8 +15,56 @@ import android.view.View;
  */
 public class DrawView extends View {
 
+    private Paint paint = new Paint();
+    private int[] spectrumBand;
+    private int offset = 400;
+    private int scale = 40;
+    private int bandNums = 16;
+
     public DrawView(Context context) {
         super(context);
+    }
+
+    public void updateCanvas(byte[] bytes){
+        spectrumBand = new int[bytes.length/2];
+        for(int i = 0; i < bytes.length / 2; i++){
+            byte rfk = bytes[2 * i];
+            byte ifk = bytes[2 * i + 1];
+            float magnitude = (float) (rfk * rfk + ifk * ifk);
+            spectrumBand[i] = (int) (10 * Math.log10(magnitude));
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //System.out.println(Main.map(spectrumBand[2], 0, 5, 0.1f, 0.7f));
+        paint.setShader(new RadialGradient(150, 150, 100, new int[]{Color.WHITE, Color.WHITE, Color.BLUE}, new float[]{0.05f, Main.map(spectrumBand[1], 0, 30, 0.1f, 0.4f), 0.8f}, Shader.TileMode.CLAMP));
+        canvas.drawRect(100, 100, 200, 200, paint);
+
+        float[] mPoints = new float[spectrumBand.length*4];
+        paint.setShader(null);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(6);
+        for (int i = 0; i < spectrumBand.length; i++) {
+            mPoints[i * 4] = i * 8;
+            mPoints[i * 4 + 1] = 0;
+            mPoints[i * 4 + 2] = i * 8;
+            int dbValue = spectrumBand[i];
+            mPoints[i * 4 + 3] = (float) (dbValue * 7);
+        }
+        canvas.drawLines(mPoints, paint);
+
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        invalidate();
+    }
+
+    public void releaseAssets(){
     }
 
     public void setDimensions(int width, int height){
--- app/src/main/java/dantech/com/audiovisual/Main.java
+++ app/src/main/java/dantech/com/audiovisual/Main.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import java.util.ArrayList;
@@ -13,13 +16,17 @@ import java.util.ArrayList;
  */
 
 public class Main extends Activity {
-    DrawView drawView;
-    int width;
-    int height;
+    private DrawView drawView;
+    private int width;
+    private int height;
+    private MediaPlayer music;
+    private Visualizer visualizer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
         drawView = new DrawView(this);
         drawView.setBackgroundColor(Color.WHITE);
@@ -32,6 +39,78 @@ public class Main extends Activity {
         drawView.setDimensions(width, height);
 
         setContentView(drawView);
+
+        music = MediaPlayer.create(this, R.raw.secrets);
+
+        setupVisualizer();
+
+        music.start();
+    }
+
+
+    private void setupVisualizer(){
+        System.out.println(Visualizer.getCaptureSizeRange()[0]+","+Visualizer.getCaptureSizeRange()[1]);
+        System.out.println(Visualizer.getMaxCaptureRate());
+        visualizer = new Visualizer(music.getAudioSessionId());
+        visualizer.setCaptureSize(128);
+        visualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+            public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
+                //drawView.updateCanvas(bytes);
+            }
+
+            public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {
+                drawView.updateCanvas(bytes);
+                //System.out.println(bytes.length);
+            }
+        }, 20000, false, true);
+        visualizer.setEnabled(true);
+
+        music.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                visualizer.setEnabled(false);
+            }
+        });
+    }
+
+    protected void onRestart(){
+        System.out.println("Restarted");
+        super.onRestart();
+    }
+
+    protected void onResume(){
+        System.out.println("Resumed");
+        super.onResume();
+    }
+
+    protected void onPause() {
+        System.out.println("Paused");
+        super.onPause();
+        if(music.isPlaying())
+            music.pause();
+    }
+
+    protected void onStop(){
+        System.out.println("Stopped");
+        super.onStop();
+    }
+
+    protected void onDestroy(){
+        System.out.println("Destroyed");
+        super.onDestroy();
+        music.release();
+        visualizer.release();
+        finish();
+    }
+
+    /*Map fuction: scales x, which has in_min and in_max range, to out_min and out_max
+    **If scaled value is outside range of out_min and out_max, make it in range*/
+    public static float map(float x, float in_min, float in_max, float out_min, float out_max){
+        float ret = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        if(ret > out_max)
+            ret = out_max;
+        if(ret < out_min)
+            ret = out_min;
+        return ret;
     }
 }
 
